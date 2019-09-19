3e589e2c15bd99a9c45683076a31ef72453044e3
Gary Tou <gtcowboybob@gmail.com>
2019-09-19 11:02:46 -0700

    Added comments and javadoc for MinMaxAccount

--- src/MinMaxAccount/MinMaxAccount.java
+++ src/MinMaxAccount/MinMaxAccount.java
@@ -1,37 +1,66 @@
 package MinMaxAccount;
 
+/*
+ * Gary Tou
+ * 9/19/2019
+ */
+
 public class MinMaxAccount extends BankingAccount {
-    private int min;
-    private int max;
+    private int min; //minimum balance ever recorded
+    private int max; //maximum balance ever recorded
 
+    /**
+     * Constructor for new MinMaxAccount and sets the min max to current balance
+     * @param s Startup
+     */
     public MinMaxAccount(Startup s){
-        super(s);
-        min = this.getBalance();
-        max = this.getBalance();
+        super(s); //Send to super class
+        min = this.getBalance(); //Set min to current balance
+        max = this.getBalance(); //Set max to current balance
     }
 
+    /**
+     * Sends debit request and udpates min max balance
+     * @param d debit request
+     */
     public void debit(Debit d){
-        super.debit(d);
-        resetMinMax();
+        super.debit(d); //send to super class which will set new balance
+        updateMinMax(); //check the new balance
     }
 
+    /**
+     * Sends credit request and updates min max balance
+     * @param c credit request
+     */
     public void credit(Credit c){
-        super.credit(c);
-        resetMinMax();
+        super.credit(c); //send to super class which will set new balance
+        updateMinMax(); //check the new balance
     }
 
-    private void resetMinMax(){
-        if(this.getBalance() < min){
+    /**
+     * Updates the minimum and maximum account balance using the current balance
+     * @return min. bal
+     */
+    private void updateMinMax(){
+        if(this.getBalance() < min){ //sets new min balance if current balance is below it
             min = this.getBalance();
-        } else if (this.getBalance() > max){
+        } else if (this.getBalance() > max){ //sets new max balance if current balance is above it
             max = this.getBalance();
         }
     }
 
+    /**
+     * Returns the minimum account balance ever recorded
+     * @return min. bal
+     */
     public int getMin(){
         return min;
     }
 
+    /**
+     * Returns the maximum account balance ever recorded
+     * @return max. bal
+     */
     public int getMax(){
         return max;
     }
